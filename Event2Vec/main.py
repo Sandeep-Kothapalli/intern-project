de2b47a9e2c9b36064c416c0997f825d203f57de
Sandeep-Kothapalli <33590559+Sandeep-Kothapalli@users.noreply.github.com>
2020-05-06 17:36:00 +0530

    Created Du user profiles (correct input format)

--- Event2Vec/main.py
+++ Event2Vec/main.py
@@ -6,21 +6,56 @@ import os
 from pprint import pprint
 
 
+class eventNode:
+    def __int__(self, event_id, latitude, longitude, event_time):
+        self.event_id = event_id
+        self.latitude = latitude
+        self.longitude = longitude
+        self.event_time = event_time
+
+    def printNode(self):
+        print(self.event_id, self.latitude, self.longitude, self.event_time)
+
+
 def main():
+    # events.csv taken from meetup api
     filePath = 'events.csv'
-    eventData = pd.read_csv(filePath)
-    # eventData.drop('description')
-    # print(eventData.head())
-    elat = eventData[['event_id', 'venue.lat']].set_index('event_id').T.to_dict('list')
-    elong = eventData[['event_id', 'venue.lon']].set_index('event_id').T.to_dict('list')
-    etime = eventData[['event_id', 'event_time']].set_index('event_id').T.to_dict('list')
-    egroup = eventData[['event_id', 'group_id']].set_index('event_id').T.to_dict('list')
+    eventData = pd.read_csv(filePath, nrows=10)
+    events = eventData['event_id'].values.tolist()
+    elat = eventData[['event_id', 'venue.lat']].set_index('event_id').T.to_dict()
+    # pprint(elat)
+    elong = eventData[['event_id', 'venue.lon']].set_index('event_id').T.to_dict()
+    # pprint(elong)
+    etime = eventData[['event_id', 'event_time']].set_index('event_id').T.to_dict()
+    # pprint(etime)
+    egroup = eventData[['event_id', 'group_id']].set_index('event_id').T.to_dict()
+    # pprint(egroup)
+    # rsvp is the event list
+    # ersvp is the event list mapped to people that attended the corresponding event
     rsvp = eventData['event_id']
-    members = [random.randint(1, 100) for i in range(100)]
+    strength = 10
+    members = [i for i in range(strength)]
     ersvp = {}
     for i in range(len(rsvp)):
-        ersvp[rsvp[i]] = [random.sample(members, random.randint(0, len(members)))]
-    pprint(elat)
+        ersvp[rsvp[i]] = random.sample(members, random.randint(0, len(members)))
+    Du = {}
+    for i in members:
+        Du[i] = []
+    for i in events:
+        attend = eventNode()
+        attend.event_id = i
+        attend.latitude = elat[i]
+        attend.longitude = elong[i]
+        attend.event_time = etime[i]
+        for j in ersvp[i]:
+            Du[j].append(attend)
+
+    # pprint(ersvp)
+    # for i in range(strength):
+    #     print(i)
+    #     for events in Du[i]:
+    #         events.printNode()
+
 
 
 if __name__ == '__main__':
